721998718848710d991236a2ce9f5aee4a320f65
LubosKrnansky <krnansky@mf.cz>
2017-10-31 17:26:49 +0100

    update PlayWave 31.10.

--- app/src/main/java/cz/androidapp/PlayWave.java
+++ app/src/main/java/cz/androidapp/PlayWave.java
@@ -27,18 +27,53 @@ public class PlayWave {
 
     public PlayWave() {
         int buffsize = AudioTrack.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
-        mAudio = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, buffsize, AudioTrack.MODE_STATIC);
+        mAudio = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, buffsize, AudioTrack.MODE_STATIC); //AudioTrack.MODE_STREAM
     }
 
     public void setWave(int delkaPeriody, byte pocetPulsu){
         //sampleCount = (int)(float)SAMPLE_RATE/frequency;
+        //double twopi = 8. * Math.atan(1.);
+        //double phase = 0.0;
         sampleCount = 2400;
         short samples[] = new short[sampleCount];
         short amplitude = 32767;
-        //double twopi = 8. * Math.atan(1.);
-        //double phase = 0.0;
-//sinus na SQUARE_ PULSE chybí
-        series = new LineGraphSeries<DataPoint>();
+
+     /*sinus wave - void generateSamples(short[] samples) {
+        double phase = getPhase();
+        for (int i = 0; i < getBufferSize(); i++) {
+            samples[i] = (short) (Synth.AMPLITUDE * Math.sin(phase));
+            phase += (Math.PI * 2) * (getFrequency() / Synth.SAMPLE_RATE);
+        }
+        setPhase(phase);
+    }*/
+
+    /*square wave - void generateSamples(short[] samples) {
+        double phase = getPhase();
+        for (int i = 0; i < getBufferSize(); i++) {
+            samples[i] = (short) (Math.signum(Math.sin(phase)) * Synth.AMPLITUDE);
+            phase += (Math.PI * 2) * (getFrequency() / Synth.SAMPLE_RATE);
+        }
+        setPhase(phase);
+    }*/
+
+    /*triangle wave - void generateSamples(short[] samples) {
+        double phase = getPhase();
+        for (int i = 0; i < getBufferSize(); i++) {
+            samples[i] = (short) ((2 / Math.PI) * Math.asin(Math.sin(phase)) * Synth.AMPLITUDE);
+            phase += (Math.PI * 2) * (getFrequency() / Synth.SAMPLE_RATE);
+        }
+        setPhase(phase);
+    }*/
+
+    /*sawtooth wave - void generateSamples(short[] samples) {
+        double phase = getPhase();
+        for (int i = 0; i < getBufferSize(); i++) {
+            samples[i] = (short) ((2 * ((phase / (2 * Math.PI)) - Math.floor(0.5 + (phase / (2 * Math.PI))))) * Synth.AMPLITUDE);
+            phase += (Math.PI * 2) * (getFrequency() / Synth.SAMPLE_RATE);
+        }
+        setPhase(phase);
+    }*/
+
         for (int i = 0; i < pocetPulsu; i++){
             for (int j = 0;j < delkaPeriody/2; j++)
             {
@@ -48,6 +83,8 @@ public class PlayWave {
 //            samples[i] = (short)(amplitude * Math.signum(phase));
 //            phase += twopi * frequency / SAMPLE_RATE;
         }
+
+        series = new LineGraphSeries<DataPoint>();
             for(int i = 0; i<sampleCount;i++){
                 series.appendData(new DataPoint(i,samples[i]),true,sampleCount);
             }
